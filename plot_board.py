9fd89ecb574dec63107d988fcc19b9cf79a2f641
Jan Szumiec <jan.szumiec@gmail.com>
2017-04-10 22:04:45 +0100

    Generate drill files as well.

--- plot_board.py
+++ plot_board.py
@@ -44,3 +44,27 @@ for layer_info in plot_plan:
     pctl.PlotLayer()
 
 pctl.ClosePlot()
+
+drlwriter = EXCELLON_WRITER( board )
+drlwriter.SetMapFileFormat( PLOT_FORMAT_PDF )
+
+mirror = False
+minimalHeader = False
+offset = wxPoint(0,0)
+# False to generate 2 separate drill files (one for plated holes, one for non plated holes)
+# True to generate only one drill file
+mergeNPTH = False
+drlwriter.SetOptions( mirror, minimalHeader, offset, mergeNPTH )
+
+metricFmt = True
+drlwriter.SetFormat( metricFmt )
+
+genDrl = True
+genMap = True
+print 'create drill and map files in %s' % pctl.GetPlotDirName()
+drlwriter.CreateDrillandMapFilesSet( pctl.GetPlotDirName(), genDrl, genMap );
+
+# One can create a text file to report drill statistics
+rptfn = pctl.GetPlotDirName() + 'drill_report.rpt'
+print 'report: %s' % rptfn
+drlwriter.GenDrillReportFile( rptfn );
